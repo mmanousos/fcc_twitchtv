61a6015f5de2b440c0d91400d8dcffc4f2304c6f
M. Manousos <mmanousos@gmail.com>
2017-12-06 13:45:53 -0800

    functional API calls

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
 $(document).ready(function() {
 
+    
     var streamers = ["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-    
-    // cycle through array of streamers to get their streams
-    // push stream data to new array
-    // cycle through new array to check if they are streaming
-    // if streaming, push link + game & status to site
-    
-    var streamData = [];
 
-    function getStream() {    
+    
+    // check if first stream is live
+        // yes, pull info & push to appropriate field on site
+        // no, move on to next streamer
+
+    function getStream() {
         for (var i = 0; i<8; i++) {
             var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + streamers[i] + '?callback=?';
-            $.getJSON(url, function(data) {
-                streamData.push(data);
-                return streamData;
-            }); 
-        }; 
-    };
+            
+            $.getJSON(url, function(data) { // access API
+                //console.log(data);
+                var stream = data.stream;
+ 
+                function streamOnline() {
+                    var name = data.stream.channel.display_name;
+                    var image = data.stream.channel.logo; 
+                    var game = data.stream.game; // get the game
+                    var link = "https://www.twitch.tv/" + name; // create the link
+                    var stream = data.stream.channel.status; // get the status
 
+                    var nameLink = "<a href='" + link + "' target='_blank'>" + name + "</a>"; // create clickable link of name
+
+                    var statusDisplay = ''; // create a string for the status
+                    statusDisplay += "Now streaming <a class='live' href='" + link + "' target='_blank'>" + game + " - " + stream + "</a>"; // fill the string with the link, game and stream
+                    
+                    var display = "<div class='row'><div class='offset-sm-1 col-sm-10'><div class='row active'><div class='col-sm-3 offset-sm-3 streamer'><p>" + nameLink + " <img class='icon' src='" + image + "' alt='streamer icon'></p></div><div class='col-sm-5 offset-sm-1 streamer-status'><span class='status'>" + statusDisplay + "</span></div></div></div></div>";
+                    $('.display-box').append(display);
+                };
+                
+                function streamOffline() {
+                    var apiLink = data._links.self;
+                    var name = apiLink.substr(37);
+                    var link = "https://www.twitch.tv/" + name; 
+                    var nameLink = "<a href='" + link + "' target='_blank'>" + name + "</a>"; // create clickable link of name
+
+                    var statusDisplay = name + " is currently offline.";
+                    
+                    var display = "<div class='row'><div class='offset-sm-1 col-sm-10'><div class='row inactive'><div class='col-sm-3 offset-sm-3 streamer'><p>" + nameLink + "</p></div><div class='col-sm-5 offset-sm-1 streamer-status'><span class='status offline'>" + statusDisplay + "</span></div></div></div></div>";             
+                    $('.display-box').append(display);        
+                };
+   
+                if (stream !== null) { // if stream is active 
+                    streamOnline();
+                } else {               // if stream is inactive 
+                    streamOffline(i); 
+                }; 
+            });    
+        }
+    }
+    
     getStream();
     
-    
-    setTimeout(function() {
-       console.log(streamData); 
-    }, 5000);
-
-    
-/* Possibly due to slow connection, array is not populating fully before executing subsequent commands. Resulting in my array calls passing through as "undefined". */    
-
-
-    
-/*    function checkStream(streamData) {
-        for (var i = 0; i<8; i++) {
-            var stream = streamData[i];     
-            if (stream !== null) {
-                console.log(stream);
-            }; 
-        };
-    };
-    checkStream();
-        
-    /*    var stream = streamData[i]
-        
-        var stream = data.stream; 
-        if (stream != null) {
-            var game = data.stream.game;
-            var link = data._links.channel;
-            var stream = data.stream.channel.status;
-            console.log(game + " - " + stream);
-            var html = ""; 
-            html += "<a class='live' href='" + link + "'>" + game + " - " + stream + "</a>";
-            $(this).find('.status').html(html); 
-        };
-             
-
-    }; */
 });
